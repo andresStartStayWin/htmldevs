a40dd4542e5cd691cf01329ce2f98ed1486934a5
Carlos A. Escobar <carlosesconav@gmail.com>
2023-09-21 16:59:48 -0500

    add navbar animation

---
+++ js/navbarHide.js
@@ -0,0 +1,14 @@
+const hiddeElement = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries)=> {
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+hiddeElement.forEach((element)=> observer.observe(element));
+
